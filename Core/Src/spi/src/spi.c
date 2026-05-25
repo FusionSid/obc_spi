bd10f41faeaa458fa89f16dbe8980635f29a511e
FusionSid <siddheshadsv@icloud.com>
2026-05-26 00:47:59 +1200

    fix: if we continously recieve bad bytes, we will never timeout due to continuing

--- Core/Src/spi/src/spi.c
+++ Core/Src/spi/src/spi.c
@@ -5,17 +5,17 @@
 #include "log.h"
 #include "packet.h"
 
-spi_status_t spi_receive_packet(SPI_HandleTypeDef *hspi, uint8_t *data_out,
-                                uint16_t *data_length_out, uint16_t data_buf_size, uint32_t timeout_ms) {
+spi_status_t spi_receive_packet(SPI_HandleTypeDef *hspi, uint8_t *data_out, uint16_t *data_length_out,
+                                uint16_t data_buf_size, uint32_t timeout_ms) {
 
     uint8_t byte;
     uint32_t start = HAL_GetTick();
 
     do {
-        if (HAL_SPI_Receive(hspi, &byte, 1, SPI_BYTE_TIMEOUT_MS) != HAL_OK) continue;
-
         if (HAL_GetTick() - start > timeout_ms)
             return SPI_ERR_TIMEDOUT; // we waited and waited and waited and nothing came :(
+
+        if (HAL_SPI_Receive(hspi, &byte, 1, SPI_BYTE_TIMEOUT_MS) != HAL_OK) continue;
 
     } while (byte != SPI_PACKET_START_BYTE);
 
@@ -63,8 +63,8 @@ spi_status_t spi_receive_packet(SPI_HandleTypeDef *hspi, uint8_t *data_out,
 }
 
 spi_status_t spi_transaction(SPI_HandleTypeDef *hspi, spi_query_code_t query_code, const uint8_t *sending_data,
-                             uint16_t sending_data_length, uint8_t *recieved_data,
-                             uint16_t *recieved_data_length, uint16_t recieving_buffer_size, uint32_t timeout_ms) {
+                             uint16_t sending_data_length, uint8_t *recieved_data, uint16_t *recieved_data_length,
+                             uint16_t recieving_buffer_size, uint32_t timeout_ms) {
     uint8_t tx_buf[SPI_PACKET_MAX_SIZE];
     uint16_t tx_buf_len = SPI_PACKET_START_AND_END_SIZE + sending_data_length;
 
@@ -80,8 +80,8 @@ spi_status_t spi_transaction(SPI_HandleTypeDef *hspi, spi_query_code_t query_cod
         return SPI_ERR_HAL;
     }
 
-    spi_status_t status = spi_receive_packet(hspi, recieved_data, recieved_data_length,
-                                             recieving_buffer_size, timeout_ms);
+    spi_status_t status =
+        spi_receive_packet(hspi, recieved_data, recieved_data_length, recieving_buffer_size, timeout_ms);
 
     spi_cs_high();
     return status;
