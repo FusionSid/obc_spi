ab29a7f27534f16f039e580187e6a7d682db8a6e
FusionSid <siddheshadsv@icloud.com>
2026-05-27 00:56:19 +1200

    feat: get RTC, ADC & RTC (test) functions for thermal payload

--- Core/Src/spi/include/test_payload.h
+++ Core/Src/spi/include/test_payload.h
@@ -5,3 +5,5 @@
 spi_status_t query_ack(SPI_HandleTypeDef *hspi);
 spi_status_t query_echo(SPI_HandleTypeDef *hspi);
 spi_status_t query_getRTD(SPI_HandleTypeDef *hspi);
+spi_status_t query_getADC(SPI_HandleTypeDef *hspi);
+spi_status_t query_getRTC(SPI_HandleTypeDef *hspi);
